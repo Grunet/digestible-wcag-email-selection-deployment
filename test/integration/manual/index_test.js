ae11b8b89783a9579e6849099e2932028afed04f
Grunet <sadikshahidain@gmail.com>
2020-09-28 10:54:25 -0500

    Renaming the "internals" to "selection_changer" which while awkward seems a little more informative

--- test/integration/manual/index_test.js
+++ test/integration/manual/index_test.js
@@ -2,4 +2,4 @@ require("dotenv").config({
   path: require("find-config")(".env", { cwd: __dirname }),
 });
 
-require("../../../packages/internals/index.js");
+require("../../../packages/selection_changer/index.js");
